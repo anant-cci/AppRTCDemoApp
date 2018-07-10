4d73e917dfb9465578e803f2c4808ad951409d8d
Anant Kannaik <anant.kannaik@creativecapsule.com>
2018-07-10 15:53:38 +0530

    Added AppRTC

---
+++ AppRTCDemoApp/ARTCVideoChatViewController.h
@@ -0,0 +1,15 @@
+//
+//  ARTCVideoChatViewController.h
+//  AppRTCDemoApp
+//
+//  Created by Anant Kannaik on 10/07/18.
+//  Copyright © 2018 Anant Kannaik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARTCVideoChatViewController : UIViewController
+
+@property (strong, nonatomic) NSString *roomName;
+
+@end
